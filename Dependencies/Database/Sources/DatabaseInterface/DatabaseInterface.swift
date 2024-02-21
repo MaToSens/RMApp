818077afb753d7b068d6bf896bfb8e59ae492b36
MaToSens <matoosens@gmail.com>
2024-02-21 16:35:30 +0100

    Implemented DatabaseManager class conforming to DatabaseManagerInterface

--- Dependencies/Database/Sources/DatabaseInterface/DatabaseInterface.swift
+++ Dependencies/Database/Sources/DatabaseInterface/DatabaseInterface.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import RealmSwift
+
+public protocol LocalDAOInterface: Object {
+    associatedtype LocalModel: LocalStorable
+    init(from: LocalModel)
+}
+
+public protocol LocalStorable: Codable, Equatable  {
+    associatedtype LocalDAO: LocalDAOInterface
+    init(from: LocalDAO)
+}
+
+public protocol DatabaseManagerInterface {
+    func save<Object: LocalStorable>(_ object: Object) async throws -> Object
+    func save<Object: LocalStorable>(_ objects: [Object]) async throws -> [Object]
+    func get<Object: LocalStorable>(type: Object.Type, primaryKey: String) async throws -> Object
+    func getAll<Object: LocalStorable>() async throws -> [Object]
+}
+
+public protocol FileStorageManagerInterface {
+    func save(
+        data: Data,
+        folderName: String,
+        fileName: String,
+        withExtension fileExtension: String
+    ) throws
+    
+    func retrieve(
+        folderName: String,
+        fileName: String,
+        withExtension fileExtension: String
+    ) throws -> Data
+}
+
+public enum FileStorageManagerError: Error {
+    case unableToAccessURL
+    case unableToConvertToData(Error)
+    case unableToCreateDirectory
+    case unableToFind
+    case unableToSave
+}
+
+public enum DatabaseManagerError: Error {
+    case unableToOpenRealm
+    case unableToSave
+    case unableToFind
+    case unableToUpdate
+    case unableToDelete
+}
