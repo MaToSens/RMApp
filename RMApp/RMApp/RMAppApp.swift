2db003ec409b9830ada387b3cfa226c627a51efc
MaToSens <matoosens@gmail.com>
2024-02-21 14:15:35 +0100

    Added initial files

---
+++ RMApp/RMApp/RMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMAppApp.swift
+//  RMApp
+//
+//  Created by MaTooSens on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
